0a14f55f142f5cb0caa59057a9e5fb6d33f0ca7c
Jerome Boisvert-Chouinard <jb@pigeonland.net>
2017-01-18 12:49:50 -0500

    Update superhero.py

--- examples/superhero_mgmt/superhero.py
+++ examples/superhero_mgmt/superhero.py
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from openerp import fields
 
-from . import odoolightwf as ltwf
+import odoolightwf as ltwf
 
 
 class SuperHero(ltwf.WorkflowModel):
